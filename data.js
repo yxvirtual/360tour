836b3aa69d44f6e08ab06dfe969519880155a0b4
yxvirtual <155383000+yxvirtual@users.noreply.github.com>
2024-01-01 12:28:51 -0500

    Upload

---
+++ data.js
@@ -0,0 +1,59 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-test1",
+      "name": "test1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.38847990039795377,
+        "pitch": 0.7579018402396489,
+        "fov": 1.1677119558089635
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.16224371096325285,
+          "pitch": 0.48494164920838223,
+          "title": "shelf",
+          "text": "color"
+        },
+        {
+          "yaw": -0.7365866809792898,
+          "pitch": 0.762771255786177,
+          "title": "massage chair",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "test",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
